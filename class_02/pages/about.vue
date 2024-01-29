e29fe877c510d87b49418fd88862c27f8fe4f38d
mdarmanbd <md.armantext@gmail.com>
2024-01-29 23:31:52 +0600

    complite the class 02

---
+++ class_02/pages/about.vue
@@ -0,0 +1,8 @@
+<script setup>
+
+</script>
+
+<template>
+    <h3>this is about page</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime dolores, mollitia sint vel rem reprehenderit perspiciatis asperiores provident delectus omnis facilis impedit ea quo modi alias temporibus nulla sapiente ab!</p>
+</template>
---
+++ class_02/pages/products/[id].vue
@@ -0,0 +1,10 @@
+<script setup>
+
+const {id} = useRoute().params
+
+</script>
+
+<template>
+    <h3>this is produt {{ id }} page</h3>
+    <p> {{ id }} Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non sint et sapiente! Id similique ut ducimus deleniti ipsum optio aliquid rerum eos! Eos dolores ullam recusandae similique minima, doloribus in.</p>
+</template>
