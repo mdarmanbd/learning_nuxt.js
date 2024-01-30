81dc78460d096f405f6428cbe68216d696e0a264
mdarmanbd <md.armantext@gmail.com>
2024-01-30 16:02:28 +0600

    add the tailwincss in nuxt project

---
+++ class_05/pages/index.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+
+</script>
+
+<template >
+
+<div class="w-full">
+    <div class="w-1/2 m-auto ">
+        <h3 class="text-red-400">index</h3>
+        <p class="text-green-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque odit dignissimos assumenda soluta, debitis eligendi doloribus repudiandae placeat obcaecati corporis quibusdam sed culpa reiciendis tenetur impedit. Quo temporibus tenetur rerum.</p>
+    </div>
+</div>
+
+   
+</template>
---
+++ class_05/pages/products/[id].vue
@@ -0,0 +1,14 @@
+<script setup>
+const {id} = useRoute().params
+// link the page in layout products.vue page
+definePageMeta({
+    layout: 'products'
+})
+
+</script>
+
+<template>
+
+    <h3> id : {{ id }}</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque odit dignissimos assumenda soluta, debitis eligendi doloribus repudiandae placeat obcaecati corporis quibusdam sed culpa reiciendis tenetur impedit. Quo temporibus tenetur rerum.</p>
+</template>
