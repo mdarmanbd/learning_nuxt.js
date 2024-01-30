c1038a2e731c07b55c0808adbe670286f8b3b060
mdarmanbd <md.armantext@gmail.com>
2024-01-30 11:40:03 +0600

    complate class 03

---
+++ class_03/pages/about.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+
+</script>
+
+<template>
+    <header>
+        <nav>
+            <ul>
+                <li><NuxtLink to="/">Home</NuxtLink></li>
+                <li><NuxtLink to="/about">About</NuxtLink></li>
+                <li><NuxtLink to="/Products">Products</NuxtLink></li>
+                <li><NuxtLink to="/Products/2">Products id</NuxtLink></li>
+            </ul>
+           <a href="/about">normal link</a>
+        </nav>
+    </header>
+
+    <h3>about</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque odit dignissimos assumenda soluta, debitis eligendi doloribus repudiandae placeat obcaecati corporis quibusdam sed culpa reiciendis tenetur impedit. Quo temporibus tenetur rerum.</p>
+</template>
---
+++ class_03/pages/products/index.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+
+</script>
+
+<template>
+
+    <h3>product index</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque odit dignissimos assumenda soluta, debitis eligendi doloribus repudiandae placeat obcaecati corporis quibusdam sed culpa reiciendis tenetur impedit. Quo temporibus tenetur rerum.</p>
+</template>
