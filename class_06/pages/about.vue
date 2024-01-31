b90e28853ae3f2519e42ca14a9b02a50b0d4031e
mdarmanbd <md.armantext@gmail.com>
2024-01-31 23:22:44 +0600

    add the class_06

---
+++ class_06/pages/about.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+
+</script>
+
+<template>
+    <header>
+      
+    </header>
+
+    <h3>about</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque odit dignissimos assumenda soluta, debitis eligendi doloribus repudiandae placeat obcaecati corporis quibusdam sed culpa reiciendis tenetur impedit. Quo temporibus tenetur rerum.</p>
+</template>
